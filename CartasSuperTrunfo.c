f69a7512495c18469e1f800e4d1d388917e383c6
TesteMRP99 <mateus.paiva99@hotmail.com>
2025-02-20 14:51:53 +0000

    inclusao da densidade populacional e PIB per capta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,18 +2,21 @@
 
 
 int main() {
-        char estado1[15], estado2[15]; //variavel que para o usuário informar o estado
-		char ccarta1[15], ccarta2[15]; //variavel para o usuário informar o código da carta;
-		char cidade1[50], cidade2[50]; //variavel para o usuário informar o nome da cidade
-		int pop1, pop2; //variavel para o usuário informar o total populacional da cidade.
-		float area1, area2;// variavel para o usuário informar o total da area em km
-		float pib1, pib2; //variavel para o usuário determinar o pib
-		int pt1, pt2; // variavel para o usuário determinar a quantidade de pontos turisticos
-	
+        char estado1[15], estado2[15]; 		//variavel que para o usuário informar o estado
+		char ccarta1[15], ccarta2[15]; 		//variavel para o usuário informar o código da carta;
+		char cidade1[50], cidade2[50]; 		//variavel para o usuário informar o nome da cidade
+		int pop1, pop2; 					//variavel para o usuário informar o total populacional da cidade.
+		float area1, area2;					// variavel para o usuário informar o total da area em km
+		float pib1, pib2; 					//variavel para o usuário determinar o pib
+		int pt1, pt2; 						// variavel para o usuário determinar a quantidade de pontos turisticos
+		float densidadep1, densidadep2; 	//variavel para calcular a densidade populacional
+		float ppc1, ppc2; 					//variavel para calcular o PIB per Capta
 		
 		printf("			Bem Vindo ao Super Trunfo!\n");
 	
-		printf("\nInforme o estado da carta 1: \n"); //a partir daqui, usuário entrará com todos os dados referente a carta 1
+
+			//a partir daqui, usuário entrará com todos os dados referente a carta 1
+		printf("\nInforme o estado da carta 1: \n");
 		scanf("%s", &estado1);
 		printf("Informe o código da carta 1: \n");
 		scanf("%s", &ccarta1);
@@ -29,8 +32,8 @@ int main() {
 		scanf("%d", &pt1);
 	
 	
-		
-		printf("\nInforme o estado da carta 2: \n"); //a partir daqui, usuário entrará com todos os dados referente a carta 1
+				//a partir daqui, usuário entrará com todos os dados referente a carta 2
+		printf("\nInforme o estado da carta 2: \n"); 
 		scanf("%s", &estado2);
 		printf("Informe o código da carta 2: \n");
 		scanf("%s", &ccarta2);
@@ -45,14 +48,32 @@ int main() {
 		printf("Infome a quantidade de pontos turisticos que contem na cidade da carta 2: \n");
 		scanf("%d", &pt2);
 	
-	
-		printf("\nDados da carta 1\n"); //saida com todas as informações digitadas pelo usuário referente a carta 1
+
+		//calculando densidade populacional da carta 1
+		densidadep1 = pop1 / area1;
+		
+		//calculando densidade populacional da carta 2
+		densidadep2 = pop2 / area2;
+		
+		//calculando PIB per capta da carta 1
+		ppc1 = pib1 / pop1;
+		
+		//calculando PIB per capta da carta 2
+		ppc2 = pib2 / pop2;
+
+
+				//saida com todas as informações digitadas pelo usuário referente a carta 1
+		printf("\nDados da carta 1\n"); 
 		printf("Estado: %s\nCódigo da carta: %s\nNome da Cidade: %s\nNúmero de Habitantes: %d\n", estado1, ccarta1, cidade1, pop1);
 		printf("Area: %.2f km²\nPIB:R$%.2f\nQuantidade de pontos turisticos: %d\n", area1, pib1, pt1);
-	
-		printf("\nDados da carta 2\n"); ////saida com todas as informações digitadas pelo usuário referente a carta 2
+		printf("Densidade Populacional: %.2f hab/Km²\n", densidadep1);
+		printf("PIB per Capta: R$%.2f de reais\n", ppc1);
+
+				//saida com todas as informações digitadas pelo usuário referente a carta 2
+		printf("\nDados da carta 2\n"); 
 		printf("Estado: %s\nCódigo da carta: %s\nNome da Cidade: %s\nNúmero de Habitantes: %d\n", estado2, ccarta2, cidade2, pop2);
 		printf("Area: %.2f km²\nPIB:R$%.2f\nQuantidade de pontos turisticos: %d\n", area2, pib2, pt2);
-
+		printf("Densidade Populacional: %.2f hab/Km²\n", densidadep2);
+		printf("PIB per Capta: R$%.2f de reais\n", ppc2);
     return 0; 
 }
